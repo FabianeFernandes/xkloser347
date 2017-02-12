dced2c5b43c4f10e647edf82b1257f3196c990af
FabianeFernandes <yani@klose.us>
2017-02-12 12:17:03 -0800

    Update lab 2.cpp
    I commented out part of the code you used for testing your functions, so I could test the implementation with the switch menu. But I didn't delete or alter any of your original stuff. It's all there. :)
    
    stringNCopy is the only piece of code that still needs work.
    
    I included: 
    -Program description + Pseudocode (top of file);
    -Function prototypes;
    -C-string variables + const int for length;
    -Implemented switch menu with available functions;
    -Added functions for single and double string input;
    -Added missing documentation to the my code;
    
    To be discussed:
    -Do we need the single string input functions I added?
    -Do we need to improve the messages being printed out to the user?
    -What else is missing?

--- lab 2.cpp
+++ lab 2.cpp
@@ -1,15 +1,125 @@
+/* Program Description: This program will offer the user a menu choice to perform C-string operations.
+ The user will choose the desired string operation, input the requested arguments and view the result
+ of the desired operations.
+ 
+ Pseudocode:
+ Display menu choices to user.
+ In a do-while loop, prompt user for a menu choice.
+ If choice is not valid:
+ Displays to user that choice was not valid.
+ Prompt if user wants to return to menu or exit.
+ If choice is valid:
+ Prompt user to imput arguments to perform operations.
+ Display results of operation to user.
+ Prompt if user wants to return to menu for new operation or exit.
+ If user requests new operation, return him back to menu.
+ Else, exists the program.
+ */
+
 #include <iostream>
 using namespace std;
 
-int stringLength(char *);
-char *stringNCopy(char *);
+//Function prototypes:
+void inputSingleString(char *string1, const int size);
+void inputDoubleString(char *string1, char *string2, const int size);
+int stringLength(char *charArrayFromMain);
+char *stringNCopy(char *stringArrayFromMain);
+char* stringAdd(char *dest, const char *src);
+int stringCompare (const char *firstWord, const char *secondWord);
+
+
+
+//int stringLength(char *);
+//char *stringNCopy(char *);
 //void stringAdd();
 //void stringCompare();
 
 
 int main()
 {
-	char choice;
+	
+    // C-string arrays for user input
+    const int LENGTH = 20;
+    char str1[40],         //double length, so str1 can hold str1 + str2
+         str2[LENGTH];
+	
+	int choice = 0;     //to hold the menu choice from 1-4
+
+    // To display the menu and get a choice from user
+    cout << "Enter the number that corresponds to the string manipulation to be performed:\n\n"
+    << "    1. String lenght.\n"
+    << "    2. String copy\n"
+    << "    3. String concatenation\n"
+    << "    4. String comparison.\n\n";
+    
+    //To give user the possibility to repeat the menu
+    char again = 'y';
+    do
+    {
+    cout << "Enter your choice and press [ENTER]: " << endl;
+    cin >> choice;
+    cin.clear();
+    cin.ignore();
+    switch (choice)
+    
+    {
+        case 1:
+        {
+            inputSingleString(str1, LENGTH);                    // single word input
+            int len = stringLength(str1);                       // return word lenght to len variable
+            cout << "Word has " << len << " characters \n";
+            break;
+        }
+            
+        case 2:
+        {
+            /*FF: This function takes 3 parameters (2 string pointers + 1 int) for the amount of
+            characters to be copied from source to destination. I used single input string
+            because your function only took a single input and I wanted to test the menu. */
+            inputSingleString(str1, LENGTH);
+            stringNCopy(str1);
+            break;
+        }
+            
+        case 3:
+        {
+            inputDoubleString(str1, str2, LENGTH);   // double word input
+            stringAdd(str1, str2);                   // returns concatenated words
+            cout << "Here are your concatenated words: " << str1 << endl;
+            break;
+        }
+            
+        case 4:
+        {
+            inputDoubleString(str1, str2, LENGTH);  // double word input
+            int comp = stringCompare (str1, str2);  // returns value of compared words
+            
+            if (comp == 0)
+                cout << "You inputed identical words.";
+            
+            else if (comp > 0)
+                cout << "The words are different: '" <<str2 << "' comes before '" << str1 << "' alphabetically\n";
+            
+            else
+            cout << "The words are different: '" <<str1 << "' comes before '" << str2 << "' alphabetically\n";
+            break;
+        }
+            
+        default: cout <<  choice << " is not a choice in this menu.\n" << "You must enter 1, 2, 3 or 4! \n";
+            
+    
+    }
+        
+    cout << "\nManipulate another string? [Y/N]";
+    cin >> again;
+    cin.clear();
+        
+    } while (again == 'y' || again == 'Y');
+    
+    return 0;
+	
+}
+	
 	//cout << "Enter A, B, or C: ";
 	//cin >> choice;
 	/*switch (choice)
@@ -32,6 +142,8 @@ int main()
 		}
 		default: cout << "You did not enter A, B, or C!\n";
 	}*/
+	
+	/*
 	cout << "sending character array to stringLength function... \n";
 	char test[99] = "12345678";
 	int stringLengthReturnValue = stringLength(test);
@@ -49,7 +161,33 @@ int main()
 	charPointer = nullptr;
 	cin.get();
 	return 0;
+}*/
+	
+
+/* Single String Input Function:
+ Accepts one input string and size limit for that string, including the null terminator.
+ cin appends a maximum of size-1 characters to string1 + automatic null terminator. */
+void inputSingleString(char *string1, const int size)
+{
+    cout << "Enter the word: " << endl;
+    cin.getline(string1, size);
+    cin.clear();
 }
+
+
+/* Double String Input Function:
+ Accepts two input strings from user and size limit for that string, including the null terminator.
+ cin appends a maximum of size-1 characters to each string variable + automatic null terminator. */
+void inputDoubleString(char *string1, char *string2, const int size)
+{
+    cout << "Enter the first word: " << endl;
+    cin.getline(string1, size);
+    cin.clear();
+    cout << "Enter the second word: " << endl;
+    cin.getline(string2, size);
+    cin.clear();
+}
+
 
 int stringLength(char *charArrayFromMain)
 {
@@ -108,36 +246,43 @@ char *stringNCopy(char *stringArrayFromMain)
 	//delete []arrayPointer;  // free up the memory again
 }
 
-//stringAdd function:
-//Duplicates string concatenation (strcat) function
+	
+/*stringAdd String Concatenation Function:
+ Duplicates C-string concatenation strcat function.
+ Acceppts two pointers to C-strings as arguments and copies the contents
+ of secondWord to firstWord.*/
 char* stringAdd(char *firstWord, const char *secondWord)
 {
     while (*firstWord!= '\0')
-        firstWord++;
+        firstWord++;			//moves the destination word pointer to the position before its null terminator
     
     while (*secondWord != '\0')
-        *firstWord++ = *secondWord++;
-    
-    return firstWord;
+        *firstWord++ = *secondWord++;	// appends the source characters to location pointed in destination word, untill
+                                        // a null terminator is found in the source word.
+    return firstWord;			// returns pointer to destination word
 
 }
 
-//stringCompare function that mimics the functionality of strcmp function
-
+	
+/* stringCompare String Comparison Function:
+ Mimics C-string camparison strcmp function.
+ Accepts two pointers to C-strings as arguments and returns an
+ int value that shows how the strings compare to each other. */
 int stringCompare (const char *firstWord, const char *secondWord)
 {
-    while( (*firstWord!='\0') && (*firstWord==*secondWord) ){
+    while( (*firstWord!='\0') && (*firstWord==*secondWord) )	// loops through firstWord until null terminator and compares
+    {                                                           // each character of firstWord to secondWor
         firstWord++;
         secondWord++;
     }
     
-    if ((*firstWord - *secondWord) > 0)
+    if ((*firstWord - *secondWord) > 0)		// firstWord is larger
         return 1;
     
-    else if ((*firstWord - *secondWord) == 0)
+    else if ((*firstWord - *secondWord) == 0)	// both words are identical
         return 0;
     
-    else
+    else					// secondWord is larger
         return -1;
     
 }
